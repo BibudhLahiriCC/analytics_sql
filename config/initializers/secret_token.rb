d3e164cb63baca253be3a77ee1b731d2186c4bfc
Bibudh Lahiri <bibudh@casecommons.org>
2012-02-15 12:38:41 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AnalyticsSql::Application.config.secret_token = '82f14fd85ca3e287f40d7cfad09b2dba5a8297390f46abcc024ba00178ae10ffc202ebd7d8d626df75bcf8a59d5a5b7d622361c65fc4452c80b910ddd5309ba9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+AnalyticsSql::Application.config.session_store :cookie_store, :key => '_analytics_sql_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# AnalyticsSql::Application.config.session_store :active_record_store
